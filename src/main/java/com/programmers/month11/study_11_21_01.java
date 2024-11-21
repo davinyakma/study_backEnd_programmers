f3f2f4fb4b3baa11d030b34531d04e6db34d0163
davinyakma <poohcdv3652@naver.com>
2024-11-21 10:42:13 +0900

    feat: 11-21_01_2_using inheritance and abstract

--- src/main/java/com/programmers/month11/study_11_21_01.java
+++ src/main/java/com/programmers/month11/study_11_21_01.java
@@ -2,6 +2,8 @@ package com.programmers.month11;
 
 public class study_11_21_01 {
     public static void main(String[] args) {
+        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
+
         샤오미Tv a샤오미Tv = new 샤오미Tv();
         삼성Tv a삼성Tv = new 삼성Tv();
         LGTv aLGTv = new LGTv();
@@ -26,49 +28,89 @@ public class study_11_21_01 {
         // 출력 => LGTv 꺼집니다.
         aLGTv.게임모드전환();
         // 출력 => LGTv 게임모드전환!
+
+        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
+
+        // 표준Tv 리모콘을 저장할 수 있는 변수를 만든다.
+        표준Tv a표준Tv;
+
+        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
+        // 중요 : 다른말로 a샤오미Tv 변수안에 있던 샤오미Tv 리코몬을 표준Tv 리모콘화 해서 a표준Tv변수에 넣는다.
+        a표준Tv = a샤오미Tv;
+        a표준Tv.켜기();
+        // 출력 : 샤오미TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 샤오미TV 꺼집니다.
+
+        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
+        a표준Tv = a삼성Tv;
+        a표준Tv.켜기();
+        // 출력 : 삼성TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 삼성TV 꺼집니다.
+
+        // a표준Tv 변수에 LGTv 객체를 연결한다.
+        a표준Tv = aLGTv;
+        a표준Tv.켜기();
+        // 출력 : LGTV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : LGTV 꺼집니다.
+
+        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
+        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
+        LGTv aLGTv2 = (LGTv) a표준Tv;
+        aLGTv2.게임모드전환();
     }
 }
 
-class 샤오미Tv { //겹치는 켜기, 끄기, vr켜기 메소드가 있으므로 코드를 더 줄이기 위해 Tv클래스에 기본 메소드 정의해서 추상화? 상속 과정이 필요함.
-    void 켜기() { //public void 를 쓰던 void만 쓰던 여기서는 중요하지 않다. 똑같은 역할이어서 생략 가능함.
+// 이 클래스는 오직 리모콘을 만들기 위해서 만들어졌습니다.
+abstract class 표준Tv {
+    abstract void 켜기(); //abstract는 메소드 안에 구현 못함. 추상화여서 내용이 작성 안 되어 있기 때문에
+    //만약 표준 Tv를 상속받는 다른 Tv들이 켜기()와 끄기()를 오버라이딩하지만 쓴다고 그러면
+    //abstract를 지우고 void 켜기()와 void 끄기()를 구현하여 쓸 수 있도록 바꿀 수 있다.
+    //하지만 표준Tv클래스는 new 표쥰Tv; 가 불가능한데 표준Tv클래스로 객체를 만들 경우가
+    //거의 없기 때문에 그냥 abstract로 클래스 타입 지정해도 좋다(오히려 추천)
+    abstract void 끄기();
+}
+
+class 샤오미Tv extends 표준Tv {
+    void 켜기() {
         System.out.println("샤오미Tv 켜집니다.");
     }
 
-    public void 끄기() {
+    void 끄기() {
         System.out.println("샤오미Tv 꺼집니다.");
     }
 
-    public void vr켜기() {
+    void vr켜기() {
         System.out.println("샤오미Tv vr켜기!");
     }
 }
 
-class 삼성Tv {
-
-    public void 켜기() {
+class 삼성Tv extends 표준Tv {
+    void 켜기() {
         System.out.println("삼성Tv 켜집니다.");
     }
 
-    public void 끄기() {
+    void 끄기() {
         System.out.println("삼성Tv 꺼집니다.");
     }
 
-    public void ar켜기() {
+    void ar켜기() {
         System.out.println("삼성Tv ar켜기!");
     }
 }
 
-class LGTv {
-
-    public void 켜기() {
+class LGTv extends 표준Tv {
+    void 켜기() {
         System.out.println("LGTv 켜집니다.");
     }
 
-    public void 끄기() {
+    void 끄기() {
         System.out.println("LGTv 꺼집니다.");
     }
 
-    public void 게임모드전환() {
+    void 게임모드전환() {
         System.out.println("LGTv 게임모드전환!");
     }
 }
