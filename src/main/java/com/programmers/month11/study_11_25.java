24dcf21acdb9d5c48cb0c40f8201c413015d37c6
davinyakma <poohcdv3652@naver.com>
2024-11-25 11:14:52 +0900

    feat: 11-25_02_1_java_map

--- src/main/java/com/programmers/month11/study_11_25.java
+++ src/main/java/com/programmers/month11/study_11_25.java
@@ -1,40 +1,47 @@
 package com.programmers.month11;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class study_11_25 {
     // 스트림 사용
     public static void main(String[] args) {
-        System.out.println("== No Stream ==");
+        System.out.println("== 리스트 ==");
+        List<Integer> ages = new ArrayList<>();
 
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // 리스트는 데이터 넣을 때 편하다.
+        ages.add(10); // 0:철수
+        ages.add(20); // 1:영수
+        ages.add(30); // 2:미애
 
-        System.out.println("= 원본 numbers(변형 전) =");
-        System.out.println(Arrays.toString(numbers));
+        // 리스트는 데이터 꺼낼 때 불편하다.
+        System.out.println(ages.get(0)); // 철수 나이
+        System.out.println(ages.get(1)); // 영수 나이
+        System.out.println(ages.get(2)); // 미애 나이
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] *= 2;
+        System.out.println("== 리스트 순회 ==");
+        for (int i = 0; i < ages.size(); i++) {
+            System.out.println(i + ":" + ages.get(i));
         }
 
-        System.out.println("= 원본 numbers(변형 후) =");
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("== 맵 ==");
+        // 데이터를 넣은 순서를 기억하고 싶다면 LinkedHashMap을 사용하면 된다.
+        Map<String, Integer> agesMap = new HashMap<>();
+        // 맵은 데이터 넣을 때 불편하다.
+        agesMap.put("철수", 10); // 철수:10
+        agesMap.put("영수", 20); // 영수:20
+        agesMap.put("미애", 30); // 미애:30
 
+        // 맵은 데이터 꺼낼 때 편하다.
+        System.out.println(agesMap.get("철수"));
+        System.out.println(agesMap.get("영수"));
+        System.out.println(agesMap.get("미애"));
 
-        System.out.println("== Stream ==");
-
-        numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        System.out.println("= 원본 numbers(스트림 전) =");
-        System.out.println(Arrays.toString(numbers));
-
-        int[] calculatedNumbers = Arrays.stream(numbers)
-                .map(e -> e * 2)
-                .toArray();
-
-        System.out.println("= 원본 numbers(스트림 후) =");
-        System.out.println(Arrays.toString(numbers));
-
-        System.out.println("= 새 배열 calculatedNumbers =");
-        System.out.println(Arrays.toString(calculatedNumbers));
+        System.out.println("== 맵 순회 ==");
+        for (String key : agesMap.keySet()) {
+            System.out.println(key + ":" + agesMap.get(key));
+        }
     }
 }
