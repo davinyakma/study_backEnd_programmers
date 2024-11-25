a5ba7af409ad60d87fa1e8518cbc1b7e1710024e
davinyakma <poohcdv3652@naver.com>
2024-11-25 10:50:03 +0900

    feat: 11-25_01_3_stream map02_remake

--- src/main/java/com/programmers/month11/study_11_25.java
+++ src/main/java/com/programmers/month11/study_11_25.java
@@ -5,30 +5,36 @@ import java.util.Arrays;
 public class study_11_25 {
     // 스트림 사용
     public static void main(String[] args) {
-        // 일반
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("== No Stream ==");
 
-        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] *= 2;
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("= 원본 numbers(변형 전) =");
+        System.out.println(Arrays.toString(numbers));
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
         }
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println("= 원본 numbers(변형 후) =");
+        System.out.println(Arrays.toString(numbers));
 
-        // 스트림 . 스트림 배열은 기존 원본배열을 수정하지 못하고 새 배열에 매핑된 결과가 저장된다.
-        arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        int[] resultArr = Arrays.stream(arr)
-                .map(e -> {
-                    return e * 2;
-                })
+        System.out.println("== Stream ==");
+
+        numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("= 원본 numbers(스트림 전) =");
+        System.out.println(Arrays.toString(numbers));
+
+        int[] calculatedNumbers = Arrays.stream(numbers)
+                .map(e -> e * 2)
                 .toArray();
 
-        resultArr = Arrays.stream(arr)
-                .map(e -> e * 2) // 이런식으로 줄여쓸 수 있다.
-                .toArray();
+        System.out.println("= 원본 numbers(스트림 후) =");
+        System.out.println(Arrays.toString(numbers));
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(resultArr));
+        System.out.println("= 새 배열 calculatedNumbers =");
+        System.out.println(Arrays.toString(calculatedNumbers));
     }
 }
