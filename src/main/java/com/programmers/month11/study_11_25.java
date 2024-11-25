0c12c242e3ce778105e3734753345696eacd2d9b
davinyakma <poohcdv3652@naver.com>
2024-11-25 12:16:46 +0900

    feat: 11-25_03_1_stream .boxed and .mapToObj

--- src/main/java/com/programmers/month11/study_11_25.java
+++ src/main/java/com/programmers/month11/study_11_25.java
@@ -1,52 +1,27 @@
 package com.programmers.month11;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class study_11_25 {
     // 스트림 사용
-    //# 문제
-    //- 원본 : new String[] {"1번", "2번", "3번"}
-    //- 결과 : new int[] {1, 3}
-    //- 조건은 짝수 제거
     public static void main(String[] args) {
-        System.out.println("== No Stream ==");
-        noStreamVersion();
+        // v1
+        List<Integer> list1 = Arrays
+                .stream(new int[]{10, 20, 30})
+                .mapToObj(e -> e) // int => Integer. 값을 바꾸면서 타입로 레퍼런스로 바꿀 수 있음.
+                .toList();
 
-        System.out.println("== Stream ==");
-        streamVersion();
+        // v2
+        List<Integer> list2 = Arrays
+                .stream(new int[]{10, 20, 30})
+                .boxed() // int => Integer. 타입만 레퍼런스로 바꿀 수 있음. mapToObj의 제한된 형태
+                .toList();
+
+        // v3 : 이 경우는 래핑(boxing)할 필요가 없다.
+        List<Integer> list3 = Arrays
+                .stream(new Integer[]{10, 20, 30}) //이미 형태가 Integer레퍼런스타입이라서 mapToObj필요 없음.
+                .toList();
     }
-
-    private static void noStreamVersion() {
-        String[] messages = new String[]{"1번", "2번", "3번"};
-        //int[] numbers = new int[]{messages.length};
-        int[] numbers = new int[messages.length];
-
-        for (int i = 0; i < messages.length; i++) {
-            String message = messages[i];
-            //"번"을 제거. message에서 가장 마지막 한자를 제거
-            message = message.substring(0, message.length() -1);
-
-            int number = Integer.parseInt(message);
-
-            if (number % 2 == 0) continue;
-            numbers[i] = number;
-            messages[i] = numbers[i] + "번";
-        }
-
-        System.out.println("= 원본 =");
-        System.out.println(Arrays.toString(messages));
-        System.out.println("= 결과 =");
-        System.out.println(Arrays.toString(numbers)); //결과가 [1,3] 이 되어야 하는데 [1,0,3]이 되는 문제가 있음. 복잡함
-    }
-
-    private static void streamVersion() {
-        int[] array = Arrays.stream(new String[]{"1번", "2번", "3번"})
-                .map(e -> e.substring(0, e.length() - 1))
-                .mapToInt(Integer::parseInt)
-                .filter(number-> number % 2 != 0)
-                .toArray();
-
-        System.out.println("= 결과 =");
-        System.out.println(Arrays.toString(array));
-    }
+    //레퍼런스 타입에서 int타입으로 매핑할 때 : mapToInt 이런식으로 사용 가능
 }
