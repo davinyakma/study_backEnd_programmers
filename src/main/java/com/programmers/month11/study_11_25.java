4f98a7ebeb726d4c311d81d5ff9524357b1440cb
davinyakma <poohcdv3652@naver.com>
2024-11-25 11:28:05 +0900

    feat: 11-25_02_2_stream map int to String

--- src/main/java/com/programmers/month11/study_11_25.java
+++ src/main/java/com/programmers/month11/study_11_25.java
@@ -1,47 +1,54 @@
 package com.programmers.month11;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 public class study_11_25 {
     // 스트림 사용
     public static void main(String[] args) {
-        System.out.println("== 리스트 ==");
-        List<Integer> ages = new ArrayList<>();
+        System.out.println("== No Stream ==");
+        noStreamVersion();
 
-        // 리스트는 데이터 넣을 때 편하다.
-        ages.add(10); // 0:철수
-        ages.add(20); // 1:영수
-        ages.add(30); // 2:미애
+        System.out.println("== Stream v1 ==");
+        streamVersionV1();
 
-        // 리스트는 데이터 꺼낼 때 불편하다.
-        System.out.println(ages.get(0)); // 철수 나이
-        System.out.println(ages.get(1)); // 영수 나이
-        System.out.println(ages.get(2)); // 미애 나이
+        System.out.println("== Stream v2 ==");
+        streamVersionV2();
+    }
 
-        System.out.println("== 리스트 순회 ==");
-        for (int i = 0; i < ages.size(); i++) {
-            System.out.println(i + ":" + ages.get(i));
+    private static void noStreamVersion() {
+        int[] numbers = new int[]{1, 2, 3};
+
+        String[] messages = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) continue;
+
+            messages[i] = numbers[i] + "번";
         }
 
-        System.out.println("== 맵 ==");
-        // 데이터를 넣은 순서를 기억하고 싶다면 LinkedHashMap을 사용하면 된다.
-        Map<String, Integer> agesMap = new HashMap<>();
-        // 맵은 데이터 넣을 때 불편하다.
-        agesMap.put("철수", 10); // 철수:10
-        agesMap.put("영수", 20); // 영수:20
-        agesMap.put("미애", 30); // 미애:30
+        System.out.println("= 원본 =");
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(messages));
+    }
 
-        // 맵은 데이터 꺼낼 때 편하다.
-        System.out.println(agesMap.get("철수"));
-        System.out.println(agesMap.get("영수"));
-        System.out.println(agesMap.get("미애"));
+    private static void streamVersionV1() {
+        Object[] array = Arrays.stream(new int[]{1, 2, 3})
+                .filter(e -> e % 2 != 0)
+                .mapToObj(e -> e + "번")
+                .toArray(); //Obj타입이여서 지정 불필요
 
-        System.out.println("== 맵 순회 ==");
-        for (String key : agesMap.keySet()) {
-            System.out.println(key + ":" + agesMap.get(key));
-        }
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void streamVersionV2() {
+        String[] array = Arrays.stream(new int[]{1, 2, 3})
+                .filter(e -> e % 2 != 0)
+                .mapToObj(e -> e + "번")
+                .toArray(String[]::new); //String타입으로 지정 필요함.
+
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
     }
 }
